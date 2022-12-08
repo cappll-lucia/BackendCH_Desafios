5e0a67c6c9aa1e80b5d8981a05bfc3c560e4eb41
lucia cappll <luciacappll@gmail.com>
2022-12-08 10:40:28 -0300

    desafio clase 10 con ejs

---
+++ desafioC10_PugEjs/engines/ejs/index.js
@@ -0,0 +1,43 @@
+
+
+const { request, response } = require('express');
+let express = require('express');
+let app = express();
+const PORT= 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set('views', './views/layout');
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+
+let Controller = require('../../components/controller');
+let controllerProds = new Controller('../../data/products.txt');
+
+
+app.get('/', (request, response, next)=>{
+    response.render('main', {});
+})
+
+app.get('/productos', async(request, response, next)=>{
+    let products = await controllerProds.getAll();
+    response.render('products', {products});
+})
+
+app.post('/productos', async(request, response, next)=>{
+    let prod = request.body;
+    if(prod){
+        await controllerProds.save(prod);
+        console.log(`Registrado con éxito`);
+        response.redirect('/');
+    }
+    else{
+        response.sendStatus(400);
+    }
+})
+
+
+app.listen(PORT, ()=>{console.log(`server running at http://localhost:${PORT}`)});
+
